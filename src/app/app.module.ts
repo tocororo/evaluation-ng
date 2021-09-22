3ecd1b77d44a6ef9879092928b2eefda0b813cd4
luiso <lecc628@gmail.com>
2021-09-22 12:47:11 -0400

    Add support for i18n
    The application needs to support Spanish and English
    languages at least.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+export function createTranslateLoader(http: HttpClient): TranslateHttpLoader
+{
+	return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		HttpClientModule,
+		TranslateModule.forRoot({
+		  loader: {
+			  provide: TranslateLoader,
+			  useFactory: (createTranslateLoader),
+			  deps: [HttpClient]
+		  }
+		}),
+		ReactiveFormsModule,
+		AppRoutingModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule
+{ }
