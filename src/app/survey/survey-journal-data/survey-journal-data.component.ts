a227508e6ead49bb7beff7f9bb1ac3f475cb4b75
luiso <lecc628@gmail.com>
2021-12-07 19:26:43 -0500

    Change url field by InputUrlComponent
    In the SurveyJournalDataComponent, the field that
    is used to write the journal url was changed
    by InputUrlComponent.

--- src/app/survey/survey-journal-data/survey-journal-data.component.ts
+++ src/app/survey/survey-journal-data/survey-journal-data.component.ts
@@ -1,8 +1,9 @@
 
 import { Component, Input, OnInit } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 
-import { ActionText, ChildControlsPath, InputContent, InputIssnComponent, InputTextComponent, TextInputAppearance } from 'toco-lib';
+import { ActionText, ChildControlsPath, InputContent, InputIssnComponent,
+	InputTextComponent, InputUrlComponent, TextInputAppearance } from 'toco-lib';
 
 import { JournalGeneralData } from '../evaluation.entity';
 
@@ -51,26 +52,8 @@ export class SurveyJournalDataComponent implements OnInit
 	@Input()
 	public journalDataFormGroup: FormGroup;
 	public nameContent: InputContent;
+	public urlContent: InputContent;
 	public issnContent: InputContent;
-
-	/**
-	 * Represents the current control that is analyzed for displaying an error. 
-	 * It is only used internally. 
-	 */
-	private _controlToDisplayError: AbstractControl;
-	/**
-	 * Represents the map of errors returned from failed validation checks. 
-	 * It is only used internally. 
-	 */
-	private _validationErrors: ValidationErrors;
-	/**
-	 * Represents the validation error of required. 
-	 */
-	private _validationError_required: string;
-	/**
-	 * Represents the validation error of invalid character. 
-	 */
-	private _validationError_invalidCharacter: string;
 
 	public constructor()
 	{
@@ -78,12 +61,8 @@ export class SurveyJournalDataComponent implements OnInit
 		this._journalData = undefined;
 		this.journalDataFormGroup = undefined;
 		this.nameContent = undefined;
+		this.urlContent = undefined;
 		this.issnContent = undefined;
-
-		this._controlToDisplayError = undefined;
-		this._validationErrors = undefined;
-		this._validationError_required = 'VAL_ERROR_REQUERIDO';
-		this._validationError_invalidCharacter = 'VAL_ERROR_CARACTER_INVAL';
 	}
 
 	public ngOnInit(): void
@@ -110,6 +89,18 @@ export class SurveyJournalDataComponent implements OnInit
 			'ariaLabel': 'NOMBRE'
 		};
 
+		this.urlContent = {
+			'formControl': InputUrlComponent.getFormControlByDefault(),
+			'name': this.journalData_ChildControlsPath.url as string,
+			'label': 'Url',
+			'controlType': InputUrlComponent,
+			'value': this._journalData.url,
+			'required': true,
+			'width': '45%',
+			'appearance': TextInputAppearance.outline,
+			'ariaLabel': 'Url'
+		};
+
 		this.issnContent = {
 			'formControl': InputIssnComponent.getFormControlByDefault(),
 			'name': this.journalData_ChildControlsPath.issn as string,
@@ -129,54 +120,11 @@ export class SurveyJournalDataComponent implements OnInit
 		);
 
 		this.journalDataFormGroup.addControl(this.journalData_ChildControlsPath.url as string,
-			new FormControl(this._journalData.url,
-				Validators.pattern(/(^|\s)((https?:\/\/)?[\w-]+(\.[\w-]+)+\.?(:\d+)?(\/\S*)?)/i)
-				// Validators.pattern(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/i)
-		));
+			this.urlContent.formControl
+		);
 
 		this.journalDataFormGroup.addControl(this.journalData_ChildControlsPath.issn as string,
 			this.issnContent.formControl
 		);
-	}
-
-	/**
-	 * Returns true if the control is in an error state; otherwise, false. 
-	 */
-	public getErrorState(controlName: Array<string | number> | string): boolean
-	{
-		this._controlToDisplayError = this.journalDataFormGroup.get(controlName);
- 
-		/* The control does not display errors before the user has a 
-		 * chance to edit the form. The checks for dirty and touched prevent errors 
-		 * from showing until the user does one of two things: changes the value, 
-		 * turning the control dirty; or blurs the form control element, setting the 
-		 * control to touched. 
-		 * Thus, it reveals an error message only if the control is invalid and 
-		 * the control is either dirty or touched. */
-		return ((this._controlToDisplayError.invalid) && (this._controlToDisplayError.dirty || this._controlToDisplayError.touched));
-	}
- 
-	 /**
-	  * Returns an error string if the control is in an error state; otherwise, empty string. 
-	  */
-	public getErrorMessage(controlName: Array<string | number> | string): string
-	{
-		this._validationErrors = (this._controlToDisplayError = this.journalDataFormGroup.get(controlName)).errors;
- 
-		/* Shows the text errors. They have a descendant order of importance. */
-		if (this._validationErrors)
-		{
-			if (this._validationErrors[Validators.pattern.name])
-			{
-				return this._validationError_invalidCharacter;
-			}
- 
-			if (this._validationErrors[Validators.required.name])
-			{
-				return this._validationError_required;
-			}
-		}
- 
-		return '';
 	}
 }
