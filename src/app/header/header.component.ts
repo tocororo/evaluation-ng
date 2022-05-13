26b8c90b95cce1e01c0f28cea1765d62f33727c8
Edel Abreu <edel.abreu@upr.edu.cu>
2022-05-13 11:17:38 -0400

    update header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { convertLangFromNumberToString } from 'toco-lib';
 
@@ -24,17 +24,85 @@ export class HeaderComponent implements OnInit {
    */
   public currentLang: number;
 
+  /**
+   * Gets a list of input `MenuElement` to build the menu of header
+   */
+  @Input()
+  public menuElements: MenuElement[];
+
+  @Input()
+  public showMenuLang: boolean;
+
   constructor(private _transServ: TranslateService) { }
 
   ngOnInit() {
     this.languageTexts = ['Español', 'English'];
     this.languageAbbrs = ['es', 'en'];
     this.currentLang = 0;  /* The default language is Spanish; that is, the zero index. */
+    this.showMenuLang == undefined ? this.showMenuLang = false : null;
+
+    this.menuElements = [
+      {
+        nameTranslate : "INICIO",
+        href : "/",
+        target :"_blank",
+        useRouterLink : true
+      },
+      {
+        nameTranslate : "ORGANIZACIONES",
+        href : "https://organizaciones.sceiba.cu",
+        target :"_blank",
+        useRouterLink : false
+      },
+      {
+        nameTranslate : "PERSONAS",
+        href : "https://personas.sceiba.cu",
+        target :"_blank",
+        useRouterLink : false
+      },
+      {
+        nameTranslate : "CATALOGO",
+        href : "https://catalogo.sceiba.cu",
+        target :"_blank",
+        useRouterLink : false
+      },
+      {
+        nameTranslate : "REVISTAS_MES",
+        href : "https://revistasmes.sceiba.cu",
+        target :"_blank",
+        useRouterLink : false
+      },
+      {
+        nameTranslate : "EVALUACION",
+        href : "https://evaluaciones.sceiba.cu",
+        target :"_blank",
+        useRouterLink : false
+      },
+      {
+        nameTranslate : "VOCABULARIO",
+        href : "https://vocabularios.sceiba.cu/es",
+        target :"_blank",
+        useRouterLink : false
+      }
+    ];
+    
     this._transServ.setDefaultLang('es');
     this._transServ.use('es');
     this._transServ.addLangs(this.languageAbbrs);
-
   }
+  
+  /*******************************************************************
+   * Check if display is less than 600px to update menu classes
+   * @returns String
+   */
+  showHideMenuHeader(): string {
+    let menuClass = 'mat-menu-trigger mat-icon-button mat-button-base';
+    return window.innerWidth < 600 ? menuClass + ' show-menu' : menuClass + ' hide-menu';
+  }
+  showHideMenuHeaderElements(): string {
+    return (window.innerWidth > 600) ? 'menu-full show-menu-elements' : 'menu-full hide-menu';
+  }
+
   /**
      * Sets the current language. 
      * @param index Zero-based index that indicates the current language. 
@@ -51,4 +119,11 @@ export class HeaderComponent implements OnInit {
     }
   }
 
+}
+
+export interface MenuElement{
+  nameTranslate: string;
+  href: string;
+  target: string;
+  useRouterLink: boolean;
 }
