248b1909ff1cdad7b05619ebfd906845a6e4b627
Rafael Martinez Estevez <malayibiri@gmail.com>
2023-03-03 03:35:47 -0500

    build

--- build.sh
+++ build.sh
@@ -1,6 +1,1 @@
 ng build  --prod --aot --preserve-symlinks --build-optimizer=true  --verbose  --baseHref https://evaluaciones.sceiba.org --deploy-url https://evaluaciones.sceiba.org/
-
-
-
- ng build  --prod --aot --preserve-symlinks --build-optimizer=true  --verbose  --baseHref ./ --deploy-url ./
-
